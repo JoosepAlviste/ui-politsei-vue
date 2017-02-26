11468ae9765dbd6c7ed7d4e5749e220a9627e33f
Hannes Liik <hannes_liik@hotmail.com>
2017-02-26 21:41:44 +0200

    Added new form imput templates for Year input and comment(textarea) input. Also added fields to Properties

---
+++ src/components/bootstrap/FormYear.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="form-group col-md-5" :class="[ errorClass, customClass, successClass ]">
+        <label class="form-control-label" :for="name" :class="{ required: required }">
+            {{ label }}
+        </label>
+
+        <input type="number"
+               class="form-control"
+               :name="name"
+               :id="name"
+               v-model="value">
+      
+        <div v-if="hasError"
+             class="form-control-feedback">
+            {{ error }}
+        </div>
+
+        <small v-if="hasHelp"
+               class="form-text text-muted">
+            {{ help_text }}
+        </small>
+    </div>
+</template>
+
+<script>
+    import Input from '../../classes/mixins/input';
+
+    export default {
+        mixins: [ Input ],
+        computed: {
+            inputClass() {
+                return (this.hasError
+                    ? 'form-control-danger'
+                    : this.hasTyped
+                        ? 'form-control-success'
+                        : ''
+                ) + ' ' + this.input_class;
+            }
+        }
+    }
+</script>
