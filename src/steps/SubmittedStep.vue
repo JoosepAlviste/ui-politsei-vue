4f200dbad987981f807d03c22d4e8041e46e2962
Brenda Uga <brendauga@gmail.com>
2017-02-27 21:30:19 +0200

    Add submitted step style

--- src/steps/SubmittedStep.vue
+++ src/steps/SubmittedStep.vue
@@ -1,17 +1,18 @@
 <template>
-    <div class="card">
+    <div class="card text-center">
 
         <div class="card-header">
             <h2 class="card-title">
-                Täname!
+                Süüteost teatamise avaldus
             </h2>
-
-            <div class="card-block">
-
-               <card-section>
-
-               </card-section>
-
+        </div>
+        <div class="list-group list-group-flush">
+            <div class="list-group-item">
+                <div class="card-block">
+                    <h4 class="card-title">Täname!</h4>
+                    <p class="card-text">Korrektse e-posti aadressi sisestamisel saabub süüteost teatamise avalduse koopia sisestatud e-posti aadressile hiljem ühe tööpäeva jooksul. Kui seda ei juhtu, võtke ühendust aadressil
+                        <a href="mailto:ppa@politsei.ee">ppa@politsei.ee</a></p>
+                </div>
             </div>
         </div>
 
